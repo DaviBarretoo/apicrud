20416517704c85435a13856ca02c813f79797db0
Davi Barreto <davibarretojkda@gmail.com>
2024-05-17 18:24:55 -0300

    update files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //config inicial 
 
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+require('dotenv').config()
+
 
 //forma de ler JSON / middlewares  recursos que são executados  entre nossas requisições e respostas.
 
@@ -20,12 +23,19 @@ app.use(express.json())
 //rota inicial / endpoint
 app.get('/',(req,res)=>{
     //mostrar req
-    
+
     res.json({
         message: 'Oi Express!'
     })
 })
 
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@apicluster.eljandh.mongodb.net/bancodaapi?retryWrites=true&w=majority&appName=ApiCluster`).then(
+    ()=> {
+console.log("conectamos ao MongoDB!")
+app.listen(3002)
+
+    }
+).catch((err)=> console.log(err))
 //entregar uma porta
 
-app.listen(3000) // disponibilizar na porta 3000
