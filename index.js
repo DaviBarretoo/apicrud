8293d5f874dafe62a0a7445eec7848cd9eb2bbdb
Davi Barreto <davibarretojkda@gmail.com>
2024-05-17 19:25:55 -0300

    simple router post

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
 
+const Person = require('./models/Person')
+
 
 //forma de ler JSON / middlewares  recursos que são executados  entre nossas requisições e respostas.
 
@@ -17,6 +19,37 @@ app.use( // Para iniciar
 //Para finalizar 
 
 app.use(express.json())
+
+// rotas da API
+app.post('/person', async (req,res) => {
+
+const {name,salary,approved} = req.body
+
+if(!name){
+    res.status(422).json( {error: 'O nome é obrigatorio'})
+}
+
+
+const person = {
+    name,
+    salary,
+    approved
+}
+
+//criando bd
+
+try{
+// Criando dados
+await Person.create(person)
+
+res.status(201).json({message:'Pessoa inserida no sistema com sucesso'})
+
+}catch(error){
+    // NÃO É uma boa pratíca de progamação mostrar o erro para o user
+    res.status(500).json({error: error})
+}
+
+})
 
 
 
